f6f379833e8199821cc57ba2446b5bf851fd2145
AlvesKZ <igoralvex01@gmail.com>
2026-02-27 14:10:57 -0300

    feat: create task entity

---
+++ src/main/java/com/noxus/task_api/entities/Task.java
@@ -0,0 +1,25 @@
+package com.noxus.task_api.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_task")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private Boolean completed;
+
+}
